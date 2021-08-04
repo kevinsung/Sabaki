0482c314dff1d2df36d95e04cd74aa385ed876e6
Kevin J. Sung <kevjsung@umich.edu>
2021-08-04 15:28:19 -0400

    support WINRATE and SCORELEAD nonstandard SGF properties

--- src/modules/sabaki.js
+++ src/modules/sabaki.js
@@ -290,14 +290,22 @@ class Sabaki extends EventEmitter {
           ...this.gameTree.listCurrentNodes(
             state.gameCurrents[state.gameIndex],
           ),
-        ].map((x) => x.data.SBKV && +x.data.SBKV[0])
+        ].map(
+          (x) =>
+            (x.data.SBKV && +x.data.SBKV[0]) ||
+            (x.data.WINRATE && Math.round(x.data.WINRATE[0] * 10000) / 100),
+        )
       },
       get scoreLeadData() {
         return [
           ...this.gameTree.listCurrentNodes(
             state.gameCurrents[state.gameIndex],
           ),
-        ].map((x) => x.data.SBKS && +x.data.SBKS[0])
+        ].map(
+          (x) =>
+            (x.data.SBKS && +x.data.SBKS[0]) ||
+            (x.data.SCORELEAD && Math.round(+x.data.SCORELEAD[0] * 100) / 100),
+        )
       },
     }
   }
